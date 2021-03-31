751750c4d5e3b06bfa8682df1154c1bbac9164c3
BertoBarreto <74553669+BertoBarreto@users.noreply.github.com>
2021-03-31 14:41:11 +0100

    Started company

---
+++ CompanyManager/Company.cs
@@ -0,0 +1,47 @@
+﻿/// <summary>
+/// Purpose: 
+/// Created By: Roberto Barreto - 21123 || Henrique Cartucho - 21122
+/// Email: a21123@alunos.ipca.pt || a21122@alunos.ipca.pt
+/// Created On: 3/31/2021 2:40:48 PM
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManager
+{
+    /// <summary>
+    /// Purpose: 
+    /// </summary>
+    public class Company
+    {
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        /// <summary>
+        /// Basic Class constructor
+        /// </summary>
+        public Company()
+        {
+
+        }
+
+        #endregion
+
+        #region METHODS
+
+        #region SETTERS
+
+        #endregion
+
+        #region GETTERS
+
+        #endregion
+
+        #endregion
+
+    }
+}
